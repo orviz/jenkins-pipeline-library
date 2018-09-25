aa50e9d8fdca06b020199d33d7becf37595127ea
Pablo Orviz <orviz@ifca.unican.es>
2018-09-25 10:21:16 +0200

    Debug site_id 13

--- vars/JiraIssueCreate.groovy
+++ vars/JiraIssueCreate.groovy
@@ -8,7 +8,7 @@ def call(site_id, project_id, summary, description, label, issue_type='Task') {
 		description: "${description}",
 		//customfield_1000: 'customValue',
 		// id or name must present for issueType.
-		issuetype: [name: "${issue_type}"],
+		issuetype: [name: "Task"],
 		labels: ["${label}"]]]
     
     response = jiraNewIssue issue: testIssue, site: "${site_id}"
