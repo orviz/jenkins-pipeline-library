cea380672aff8d8fa4b6c874867c102991be27e5
Pablo Orviz <orviz@ifca.unican.es>
2018-09-25 11:07:08 +0200

    Add assignee

--- vars/JiraIssueCreate.groovy
+++ vars/JiraIssueCreate.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/groovy
 
-def call(site_id, project_id, summary, description, label, issue_type) {
+def call(site_id, project_id, summary, description, label, issue_type, assignee) {
 	def testIssue = [fields: [ 
 		// id or key must present for project.
 		project: [id: "${project_id}"],
@@ -9,7 +9,8 @@ def call(site_id, project_id, summary, description, label, issue_type) {
 		//customfield_1000: 'customValue',
 		// id or name must present for issueType.
 		issuetype: [name: "${issue_type}"],
-		labels: ["${label}"]]]
+		labels: ["${label}"],
+        assignee: [name:"${assignee}"]]]
     
     response = jiraNewIssue issue: testIssue, site: "${site_id}"
 
