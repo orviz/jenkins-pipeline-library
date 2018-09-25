1b83f0b60dd5bc59dc16ce97d6f495ed253b607f
Pablo Orviz <orviz@ifca.unican.es>
2018-09-25 10:17:21 +0200

    Debug site_id 11

--- vars/JiraIssueCreate.groovy
+++ vars/JiraIssueCreate.groovy
@@ -3,16 +3,16 @@
 def call(site_id, project_id, summary, description, label, issue_type='Task') {
 	def testIssue = [fields: [ 
 		// id or key must present for project.
-		project: [id: project_id],
-		summary: summary,
-		description: description,
+		project: [id: '${project_id}'],
+		summary: '${summary}',
+		description: '${description}',
 		//customfield_1000: 'customValue',
 		// id or name must present for issueType.
-		issuetype: [name: issue_type],
-		labels: [label]]]
+		issuetype: [name: '${issue_type}'],
+		labels: ['${label}']]]
     println "${issue_type}"
     
-    response = jiraNewIssue issue: testIssue, site: site_id
+    response = jiraNewIssue issue: testIssue, site: '${site_id}'
 
 	echo response.successful.toString()
 	echo response.data.toString()
